4295e148fd01ee389242963bd28a434bea3d00a6
Gorp <gorpdev@gmail.com>
2026-04-09 23:33:20 -0400

    removed more warnings

--- src/data/data_utils.py
+++ src/data/data_utils.py
@@ -151,7 +151,7 @@ class MemmapDataset:
         mm = self._get_memmap(file_id)
         spec = mm[start:start + length]  # (time, freq)
 
-        spec = torch.from_numpy(spec).float().T
+        spec = torch.from_numpy(spec.copy()).float().T
 
         # ---------------------------
         # Generate views
