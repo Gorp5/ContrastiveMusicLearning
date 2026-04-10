fixed cls rotation

src/individual_ablation_training.py
@@ -59,11 +59,11 @@ def build_dataloader(dataset_path, batch_size, chunk_length):
         dataset,
         batch_size=batch_size,
         shuffle=True,
-        num_workers=0,              # 🔥 FIX: stop CPU explosion
+        num_workers=2,
         pin_memory=True,
         drop_last=True,
-        # persistent_workers=True,    # 🔥 reduces worker restart overhead
-        # prefetch_factor=2           # 🔥 prevents CPU starvation spikes
+        persistent_workers=True,
+        prefetch_factor=2
     )
 
 
@@ -74,7 +74,7 @@ def gpu_worker(gpu_id, args, model_params_list):
     print(f"[GPU {gpu_id}] worker starting")
 
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
-    device = torch.device("cpu")
+    device = torch.device("cuda:0")
 
     # torch.cuda.set_device(device)
 
@@ -215,15 +215,14 @@ if __name__ == "__main__":
         })
 
     processes = []
-    gpu_worker(0, args, models_per_gpu)
+    # gpu_worker(0, args, models_per_gpu)
+    for gpu_id in range(args.num_gpus):
+        p = mp.Process(
+            target=gpu_worker,
+            args=(gpu_id, args, models_per_gpu)
+        )
+        p.start()
+        processes.append(p)
 
-    # for gpu_id in range(args.num_gpus):
-    #     p = mp.Process(
-    #         target=gpu_worker,
-    #         args=(gpu_id, args, models_per_gpu)
-    #     )
-    #     p.start()
-    #     processes.append(p)
-    #
-    # for p in processes:
-    #     p.join()
+    for p in processes:
+        p.join()